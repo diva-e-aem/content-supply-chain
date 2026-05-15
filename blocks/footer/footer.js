d92b9f19f2b174b94e32994641f23c640dd8c7c0
iza17hor <iza17hor@gmail.com>
2026-05-15 16:03:49 +0200

    footer

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,33 +1,3 @@
-import { getMetadata } from '../../scripts/aem.js';
-import { loadFragment } from '../fragment/fragment.js';
-
-export default async function decorate(block) {
-  const footerMeta = getMetadata('footer');
-
-  const footerPath = typeof footerMeta === 'string' && footerMeta.startsWith('/')
-    ? footerMeta
-    : '/content/site/footer';
-
-  let fragment;
-
-  try {
-    fragment = await loadFragment(footerPath);
-  } catch (e) {
-    console.warn('Footer fragment failed to load:', footerPath, e);
-    return;
-  }
-
-  if (!fragment || !fragment.firstElementChild) {
-    return;
-  }
-
-  block.textContent = '';
-
-  const footer = document.createElement('div');
-
-  while (fragment.firstElementChild) {
-    footer.append(fragment.firstElementChild);
-  }
-
-  block.append(footer);
+export default function decorate(block) {
+  block.innerHTML = '... render footer from JS block ...';
 }
